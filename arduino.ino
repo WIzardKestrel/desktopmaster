02fb446662be5c20abb291c9d87a9fa1c8468f06
wk85 <41642920+WIzardKestrel@users.noreply.github.com>
2021-11-27 02:01:43 +0300

    Create arduino.ino
    this code controls the LEDs and sends volume data to the computer

---
+++ arduino.ino
@@ -0,0 +1,101 @@
+int ledPin[] = {13,12,11,10,9,8,7,6,5,4}; // the pin that the LED is attached to
+int sound_lvl;
+int cont_lvl;
+int pot_pin = A1;
+int old_vol_lvl = 0;
+void setup() {
+  // initialize serial communication:
+  Serial.begin(9600);
+  // initialize the LED pin as an output:
+  for (int i = 13; i > 3; i--) {
+    pinMode(i, OUTPUT);
+  }
+}
+
+
+void loop() {
+
+  cont_lvl = (analogRead(pot_pin));
+  if (abs(cont_lvl - old_vol_lvl) > 20) {
+    old_vol_lvl = cont_lvl;
+    Serial.println(cont_lvl);
+  }
+
+  if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    sound_lvl = Serial.read();
+    if (sound_lvl == ' ' || sound_lvl == '\n')
+      ;
+    else {
+      //Serial.print('\n');
+      switch (sound_lvl) {
+        case '0':
+          turn_on(10);
+          break;
+        case '1':
+          turn_on(1);
+          break;
+
+        case '2':
+          turn_on(2);
+          break;
+
+        case '3':
+          turn_on(3);
+          break;
+
+        case '4':
+          turn_on(4);
+          break;
+
+        case '5':
+          turn_on(5);
+          break;
+
+        case '6':
+          turn_on(6);
+          break;
+
+        case '7':
+          turn_on(7);
+          break;
+
+        case '8':
+          turn_on(8);
+          break;
+
+        case '9':
+          turn_on(9);
+          break;
+      }
+    }
+  }
+}
+
+void turn_on(int level) {
+  for (int i = 0; i < level; i++) {
+    digitalWrite(ledPin[i], HIGH);
+  }
+  for (int i = 14; i >= level; i--) {
+    digitalWrite(ledPin[i], LOW);
+  }
+}
+// // the pin that the LED is attached to
+//int sound_level;      // a variable to read incoming serial data into
+//
+//void setup() {
+//  // initialize serial communication:
+//  Serial.begin(9600);
+//  // initialize the LED pin as an output:
+
+//}
+//
+//void loop() {
+//
+//  if (Serial.available() > 0) {
+//    // read the oldest byte in the serial buffer:
+//    sound_level = Serial.read();
+//    printf("sound: %d\n", sound_level);
+
+//}
+
